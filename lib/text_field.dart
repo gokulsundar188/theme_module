42d98ad2181982559d793024b3ffbd3de4eaf743
Gokul Sundar <gokul.sundar@golden-hippo.com>
2020-12-23 19:55:40 +0530

    dense value change

--- lib/text_field.dart
+++ lib/text_field.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class TextFormFieldThemes {
   InputDecoration textFieldTheme() {
     return InputDecoration(
+      isDense: true,
       contentPadding: EdgeInsets.all(10),
         border: OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
         enabledBorder:
