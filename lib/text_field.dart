592fd1757aaa963283b4a39b83c76259c6bbbe39
Gokul Sundar <gokul.sundar@golden-hippo.com>
2020-12-23 19:54:36 +0530

    changes

--- lib/text_field.dart
+++ lib/text_field.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class TextFormFieldThemes {
   InputDecoration textFieldTheme() {
     return InputDecoration(
+      contentPadding: EdgeInsets.all(10),
         border: OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
         enabledBorder:
             OutlineInputBorder(borderSide: BorderSide(color: Colors.black)));
